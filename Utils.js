c6a6e98c72ce7573985958814223f592d0275efe
MustafaKrc <100158429+MustafaKrc@users.noreply.github.com>
2023-09-19 17:38:07 +0300

    delete files from last commit

--- Utils.js
+++
@@ -1,6 +0,0 @@
-function notify(notifier, notificationText, imageSource, notificationColor) {
-    if(notifier === undefined || notifier === null)
-        return
-
-    notifier.open(notificationText, imageSource, notificationColor)
-}
